c1a0658d935cec105bdf38019f42bb5cc7277c4e
lassejlv <vestergaardlasse2@gmail.com>
2026-01-26 00:54:19 +0100

    Add

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -79,6 +79,14 @@ export const Route = createRootRoute({
         'data-website-id': '31f1090c-9170-46df-aa85-59842cc2dfb1',
         async: true,
       },
+      {
+        content: `window.$ujq=window.$ujq||[];window.uj=window.uj||new Proxy({},{get:(_,p)=>(...a)=>window.$ujq.push([p,...a])});document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://cdn.userjot.com/sdk/v2/uj.js',type:'module',async:!0}));`,
+      },
+      {
+        content: `window.uj.init('cmkon0iqi055g15moaxmxwn4g', {
+          widget: true
+        });`,
+      },
     ],
   }),
 
