5fe6b6b196ed7abbfe59eb5c5cd2c9dfe730517c
lassejlv <vestergaardlasse2@gmail.com>
2026-01-26 23:48:16 +0100

    Use new serverless driver

--- web/drizzle/index.ts
+++ web/drizzle/index.ts
@@ -1,7 +1,12 @@
 import { env } from '@/lib/env'
-import { drizzle } from 'drizzle-orm/bun-sql'
+import { neon, neonConfig } from '@neondatabase/serverless'
+import { drizzle } from 'drizzle-orm/neon-http'
 
 import * as authSchema from './schema/auth'
 import * as deviceSchema from './schema/device'
 
-export const db = drizzle(env.DATABASE_URL, { schema: { ...authSchema, ...deviceSchema } })
+// This MUST be set for PlanetScale Postgres connections
+neonConfig.fetchEndpoint = (host) => `https://${host}/sql`
+const sql = neon(env.DATABASE_URL)
+
+export const db = drizzle(sql, { schema: { ...authSchema, ...deviceSchema } })
--- web/src/lib/env.ts
+++ web/src/lib/env.ts
@@ -14,6 +14,13 @@ const envSchema = z.object({
   GITHUB_CLIENT_SECRET: z.string(),
   TURNSTILE_SECRET_KEY: z.string(),
   TURNSTILE_SITE_KEY: z.string(),
+
+  // New Serverless Driver
+  //DATABASE_HOST: z.string().min(1),
+  //DATABASE_PORT: z.coerce.number().default(5432),
+  //DATABASE_NAME: z.string().min(1),
+  //DATABASE_USERNAME: z.string().min(1),
+  //DATABASE_PASSWORD: z.string().min(1),
 })
 
 export const env = envSchema.parse(process.env)
