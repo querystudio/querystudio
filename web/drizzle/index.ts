a7f2310245ccf9bc82a0e39eac38ce585a8f456f
lassejlv <vestergaardlasse2@gmail.com>
2026-01-31 13:12:38 +0100

    Update index.ts

--- web/drizzle/index.ts
+++ web/drizzle/index.ts
@@ -2,6 +2,5 @@ import { env } from "@/lib/env";
 import { drizzle } from "drizzle-orm/bun-sql";
 
 import * as authSchema from "./schema/auth";
-import * as deviceSchema from "./schema/device";
 
-export const db = drizzle(env.DATABASE_URL, { schema: { ...authSchema, ...deviceSchema } });
+export const db = drizzle(env.DATABASE_URL, { schema: { ...authSchema } });
